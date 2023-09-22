937a1ad341ef4e8e2f4e1cb6b1d8837411da2c1e
Mikhail <m.zavarzin@primecore.io>
2023-09-22 20:07:14 +0300

    login form

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LogIn from '../views/LogIn.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    
     {
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/log-in',
+      name: 'login',
+      component: LogIn
     },
     {
       path: '/about',
